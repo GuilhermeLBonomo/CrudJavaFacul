0fbb6c3a6fdd299ca2cc9033394882fbd475b534
ADS 2023.2N <36129382023.2N@senacrjedu.local>
2024-09-26 21:38:30 -0300

    26/09 21:38

--- clima/clima/src/main/java/com/senac/clima/repositories/MunicipioRepository.java
+++ clima/clima/src/main/java/com/senac/clima/repositories/MunicipioRepository.java
@@ -1,9 +1,15 @@
 package com.senac.clima.repositories;
 
 import com.senac.clima.entities.Municipio;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MunicipioRepository extends JpaRepository<Municipio, Long> {
+    @Transactional
+    @Query(value = "UPDATE municipio SET municipio_status = -1 WHERE municipio_id = :id", nativeQuery = true)
+    void apagarLogicoMunicipioPorId(@Param("id") Long id);
 }
